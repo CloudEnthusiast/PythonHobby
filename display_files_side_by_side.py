943ba5358e485338c67d43f89f0f52d9bd2b62e6
CloudEnthusiast <31588333+CloudEnthusiast@users.noreply.github.com>
2017-09-25 15:13:07 +0530

    display files side by side in terminal

---
+++ display_files_side_by_side.py
@@ -0,0 +1,30 @@
+import sys
+
+# Specify the number of spaces between the columns
+S = 4
+
+try:
+    sys.argv[2]=''
+except:
+   print '\nSecond file name is needed to compare'
+   sys.exit(0)
+
+# Read the first file
+f1 = open( sys.argv[1] ).read().split('\n')
+
+# Read the second file
+f2 = open( sys.argv[2] ).read().split('\n')
+
+# Find the length of the longest line of the first file
+n = len(max(f1, key=len))
+
+# Print the lines
+for i in  xrange( max(len(f1), len(f2)) ):
+
+    try:
+        print f1[i] + ' '*( n - len(f1[i]) + S) + f2[i]
+    except:
+        try:
+            print ' ' + ' '*( n - 1 + S) + f2[i]
+        except:
+            print f1[i]
